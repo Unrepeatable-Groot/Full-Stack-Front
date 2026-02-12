eb166cdeea701d5620fa2fc9f83c3bdd3844f0ce
Unrepeatable-Groot <gaikarashviligiorgi5@gmail.com>
2026-02-12 15:42:39 +0400

    Search Engine

--- src/app/searched/shared/http.service.ts
+++ src/app/searched/shared/http.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class HttpService {
 
-  private apiUrl = 'http://127.0.0.1:5000/api';
+  private apiUrl = 'https://full-stack-back-a5er.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
